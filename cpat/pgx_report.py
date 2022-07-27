add9e3229e4119bed6c76f1bca60a8a0fd216715
YaqingLiu <yaqing.liu@outlook.com>
2022-07-27 11:17:20 +0800

    style: font size in

--- cpat/pgx_report.py
+++ cpat/pgx_report.py
@@ -60,11 +60,11 @@ def report(race, pgx_summary, dic_diplotype, clinical_anno_table, dosing_guideli
         <img src="%s" title="CPAT">
       </a>
     </h1>
-    <p class="lead">
+    <p class="head_lead">
       An automated Clinical Pharmacogenomics Annotation Tool to report drug responses and prescribing recommendations by parsing the germline variants.
     </p>
     <blockquote>
-      <p><b>Sample ID:</b> %s<br><b>Biogeographic Group:</b> %s<br><b>Report Time:</b> %s</p>
+      <p>Sample ID: %s<br>Biogeographic Group: %s<br>Report Time: %s</p>
     </blockquote>
     """
     print(basic_info%(logo1_fp, sample_id, race, time.asctime(time.localtime(time.time()))), file=f)
@@ -79,8 +79,8 @@ def report(race, pgx_summary, dic_diplotype, clinical_anno_table, dosing_guideli
 
     ## Part 2: Pharmacogenomics Annotation
     part2_header = """
-    <h2 id="summary">PGx Summary</h2>
-    <p class="lead">CPAT combines genotypes with PharmGKB's clinical annotations to summarise a patient's response to a specific drug across four dimensions, including <font color="#333"><b>toxicity</b></font>, <font color="#333"><b>dosage</b></font>, <font color="#333"><b>efficacy</b></font>, <font color="#333"><b>metabolism/PK</b></font>. Response levels are indicated using <font color="#8E529A"><b>decreased</b></font>, <font color="#653F92"><b>moderate</b></font> and <font color="#44308d"><b>increased</b></font>. Results with variant-specific prescribing guidance in professional clinical guidelines or FDA-approved drug label annotations will be labeled as <font color="#54A052"><b>Level A</b></font>, otherwise as <font color="#3978B1"><b>Level B</b></font>.</p>
+    <h2 id="summary"><b>PGx Summary</b></h2>
+    <p class="main_lead">CPAT combines genotypes with PharmGKB's clinical annotations to summarise a patient's response to a specific drug across four dimensions, including <font color="#444"><b>toxicity</b></font>, <font color="#444"><b>dosage</b></font>, <font color="#444"><b>efficacy</b></font>, <font color="#444"><b>metabolism/PK</b></font>. Response levels are indicated using <font color="#8E529A"><b>decreased</b></font>, <font color="#653F92"><b>moderate</b></font> and <font color="#44308d"><b>increased</b></font>. Results with variant-specific prescribing guidance in professional clinical guidelines or FDA-approved drug label annotations will be labeled as <font color="#54A052"><b>Level A</b></font>, otherwise as <font color="#3978B1"><b>Level B</b></font>.</p>
     """
     print(part2_header, file=f)
     
@@ -110,7 +110,7 @@ def report(race, pgx_summary, dic_diplotype, clinical_anno_table, dosing_guideli
     categories = ['Toxicity', 'Dosage', 'Efficacy', 'Metabolism/PK']
     fa = dict(zip(categories, ['fa-skull-crossbones', 'fa-pills', 'fa-vial-circle-check', 'fa-disease', 'fa-feather-pointed']))
     for cat in categories:
-      print('<h3 id="%s"><i class="fa-solid %s"></i> %s</h3>' % (cat.lower(), fa[cat], cat), file=f)
+      print('<h3 id="%s"><i class="fa-solid %s"></i><b> %s</b></h3>' % (cat.lower(), fa[cat], cat), file=f)
       i = 0
       html_input = []
       cat_pgx = pgx_summary[pgx_summary.PhenotypeCategory == cat]
@@ -131,11 +131,11 @@ def report(race, pgx_summary, dic_diplotype, clinical_anno_table, dosing_guideli
                           html_input[3], html_input[4], html_input[5]), file=f)
       
     ## Part 3: Dosing Guideline
-    print('<h2 id="guideline">Dosing Guideline</h2>', file=f)
-    print('<p class="lead">CPAT integrates brief annotations of genotype-based dosing recommendations after PharmGKB processing. Original PGx-based drug dosing guidelines include the <a href="http://cpicpgx.org/">Clinical Pharmacogenetics Implementation Consortium</a> (CPIC), the <a href="https://www.knmp.nl/dossiers/farmacogenetica/">Royal Dutch Association for the Advancement of Pharmacy - Pharmacogenetics Working Group</a> (DPWG), the <a href="https://cpnds.ubc.ca/">Canadian Pharmacogenomics Network for Drug Safety</a> (CPNDS), the French National Network for Pharmacogenetics (RNPGx), The Australian and New Zealand consensus guidelines (AusNZ), the Spanish Pharmacogenetics and Pharmacogenomics Society (SEFF), the Cystic Fibrosis Foundation (CFF), the American College of Rheumatology.</p>', file=f)
+    print('<h2 id="guideline"><b>Dosing Guideline</b></h2>', file=f)
+    print('<p class="main_lead">CPAT integrates brief annotations of genotype-based dosing recommendations after PharmGKB processing. Original PGx-based drug dosing guidelines include the <a href="http://cpicpgx.org/">Clinical Pharmacogenetics Implementation Consortium</a> (CPIC), the <a href="https://www.knmp.nl/dossiers/farmacogenetica/">Royal Dutch Association for the Advancement of Pharmacy - Pharmacogenetics Working Group</a> (DPWG), the <a href="https://cpnds.ubc.ca/">Canadian Pharmacogenomics Network for Drug Safety</a> (CPNDS), the French National Network for Pharmacogenetics (RNPGx), The Australian and New Zealand consensus guidelines (AusNZ), the Spanish Pharmacogenetics and Pharmacogenomics Society (SEFF), the Cystic Fibrosis Foundation (CFF), the American College of Rheumatology.</p>', file=f)
     # Drug - Detected variant or alleles - Dosing guidelines
     for drug in list(dosing_guideline_table.Drug.drop_duplicates()):
-      print('<h3>%s</h3>' % drug, file=f)
+      print('<h3><b>%s</b></h3>' % drug, file=f)
       drug_sub = dosing_guideline_table[dosing_guideline_table.Drug == drug]
       for gene in list(drug_sub.Gene.drop_duplicates()):
         drug_by_gene = drug_sub[drug_sub.Gene == gene]
@@ -151,10 +151,10 @@ def report(race, pgx_summary, dic_diplotype, clinical_anno_table, dosing_guideli
             print('<div class="alert alert-info-blue"><a href=%s target="_blank"><i class="fa-solid fa-circle-info"></a></i><b> %s: </b>%s</div>' % (row.DosingURL, row.DosingSource, row.DosingAnnotation), file=f)
     
     ## Part 4: Diplotype Detail
-    print('<h2 id="diplotype">Diplotype Detail</h2>', file=f)
-    print('<p class="lead">CPAT predicts the diplotypes for PGx genes with star (*) or named allele information, which include ABCG2, CACNA1S, CFTR, CYP2B6, CYP2C8, CYP2C9, CYP2C19, CYP3A4, CYP3A5, CYP4F2, DPYD, G6PD, MT-NR1, NUDT15, RYR1, SLCO1B1, TPMT, UGT1A1, and VKORC1. CPAT assumes that no variation occurs for the missing positions in the submitted VCFs, and the final inferred diplotypes are a composite ranking result.</p>', file=f)
+    print('<h2 id="diplotype"><b>Diplotype Detail</b></h2>', file=f)
+    print('<p class="main_lead">CPAT predicts the diplotypes for PGx genes with star (*) or named allele information, which include ABCG2, CACNA1S, CFTR, CYP2B6, CYP2C8, CYP2C9, CYP2C19, CYP3A4, CYP3A5, CYP4F2, DPYD, G6PD, MT-NR1, NUDT15, RYR1, SLCO1B1, TPMT, UGT1A1, and VKORC1. CPAT assumes that no variation occurs for the missing positions in the submitted VCFs, and the final inferred diplotypes are a composite ranking result.</p>', file=f)
     for gene in list(dic_diplotype.keys()):
-      print('<h3>%s: %s</h3>' % (gene, dic_diplotype[gene]['step2_res']), file=f)
+      print('<h3><b>%s: %s</b></h3>' % (gene, dic_diplotype[gene]['step2_res']), file=f)
       gene_detail = dic_diplotype[gene]['detail']
       header = '<table id="customer_table" border="1" cellspacing="0">\n<tr><th>Position</th><th width="120px">rsID</th><th width="200px">Effect on Protein</th><th width="275px">Definition of Alleles</th><th width="200px">Detected Alleles</th></tr>'
       for pos_res in gene_detail:
@@ -164,8 +164,8 @@ def report(race, pgx_summary, dic_diplotype, clinical_anno_table, dosing_guideli
       print(header, file=f)
     
     ## Part 5: PGx Annotation
-    print('<h2 id="annotation">PGx Annotation</h2>', file=f)
-    print('<p class="lead">CPAT annotates PGx-related single-locus genotypes and predicted diplotypes using the knowledge from PharmGKB, which are the basis for calculating the PGx summary results.</p>', file=f)
+    print('<h2 id="annotation"><b>PGx Annotation</b></h2>', file=f)
+    print('<p class="main_lead">CPAT annotates PGx-related single-locus genotypes and predicted diplotypes using the knowledge from PharmGKB, which are the basis for calculating the PGx summary results.</p>', file=f)
     detail = clinical_anno_table.drop(columns=['Class']).reset_index(drop = True)
     header = '<table id="customer_table" border="1" cellspacing="0">\n<tr><th width="60px"></th><th>Gene</th><th>Variant</th><th>Alleles</th><th>Drug</th><th>Evidence</th><th>Category</th><th>Function</th></tr>'
     for index, row in detail.iterrows():
@@ -175,8 +175,8 @@ def report(race, pgx_summary, dic_diplotype, clinical_anno_table, dosing_guideli
     
     # Part 4: Disclaimers
     disclaimer = """
-    <h2 id="about">Disclaimers</h2>
-    <p class="lead">
+    <h2 id="about"><b>Disclaimers</b></h2>
+    <p class="main_lead">
       The report incorporates analyses of peer-reviewed studies and other publicly available information identified by CPAT by State Key Laboratory of Genetic Engineering from the School of Life Sciences and Human Phenome Institute, Fudan University, Shanghai, China. These analyses and information may include associations between a molecular alteration (or lack of alteration) and one or more drugs with potential clinical benefit (or potential lack of clinical benefit), including drug candidates that are being studied in clinical research.<br>
       <em>Note:</em> A finding of biomarker alteration does not necessarily indicate pharmacologic effectiveness (or lack thereof) of any drug or treatment regimen; a finding of no biomarker alteration does not necessarily indicate lack of pharmacologic effectiveness (or effectiveness) of any drug or treatment regimen.<br>
       <em>No Guarantee of Clinical Benefit:</em> This Report makes no promises or guarantees that a particular drug will be effective in the treatment of disease in any patient. This report also makes no promises or guarantees that a drug with a potential lack of clinical benefit will provide no clinical benefit.<br>
